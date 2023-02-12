fe2d45e9a44bdf29238ab59a9af32cfaf1b6f65d
lucho_latella <lucho_latella@hotmail.com>
2023-02-11 21:11:24 -0300

    Agregar cta bancaria

---
+++ PrjCuentaBancaria/src/main/java/com/Lab/dominio/CuentaBanco.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Lab.dominio;
+
+/**
+ *
+ * @author LUCIANOLATELLA
+ */
+public class CuentaBanco {
+    //definición atributos
+    
+    private int codcuenta;
+    private float saldo;
+    private String titular;
+    
+    //definir constructor
+    public CuentaBanco(){
+        codcuenta = 0;
+        saldo = 0;
+        titular = "Sin definir";
+    }
+}
---
+++ PrjCuentaBancaria/src/main/java/com/mycompany/prjcuentabancaria/PrjCuentaBancaria.java
@@ -0,0 +1,25 @@
+    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.prjcuentabancaria;
+
+import com.Lab.dominio.CuentaBanco;
+import java.util.Scanner;
+
+/**
+ *
+ * @author LUCIANOLATELLA
+ */
+public class PrjCuentaBancaria {
+
+    public static void main(String[] args) {
+        System.out.println("Proyecto Cuenta Bancaria \n");
+        Scanner Teclado = new Scanner(System.in);
+        CuentaBanco MiCuenta = new CuentaBanco();
+        
+        System.out.print("Mi código de cuenta bancaria es ");
+        
+    }
+}
